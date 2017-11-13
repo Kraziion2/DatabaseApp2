31dcd5f7419b39f135eb9a4d18b82ddc3954664c
Bartosz G <supersamiec2012@gmail.com>
2017-11-13 14:48:12 +0000

    fuck this shit

--- AndroidStudioProjects/DatabaseApp/app/src/main/java/com/example/bartosz/databaseapp/Constants.java
+++ AndroidStudioProjects/DatabaseApp/app/src/main/java/com/example/bartosz/databaseapp/Constants.java
@@ -11,4 +11,7 @@ public class Constants {
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
 
     public static  final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+    //fuck this shit
+
 }
